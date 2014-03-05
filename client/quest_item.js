cab9d4baf511e46a79d4844fad6a91e951eb225e
Michael Conlin <conlinm@me.com>
2014-03-05 00:24:46 -0800

    latest updates

---
+++ client/quest_item.js
@@ -0,0 +1,11 @@
+// Template.questItem.events({
+//     'click .btn': function (e, tmpl) {
+//       var selectedAnswer = this.value;
+//       return selectedAnswer;
+//     }
+//   });
+
+setAnswer = function (v) {
+	var selectedAnswer = v;
+      	return selectedAnswer;
+}
