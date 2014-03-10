10bb4d00852c44f5719ac659d80f8e2dbdfa7d91
Michael Conlin <conlinm@me.com>
2014-03-10 02:13:49 -0700

    new stuff

--- client/quest_item.js
+++ client/quest_item.js
@@ -1,17 +1,33 @@
-// Template.questItem.events({
-//     'click .btn': function (e, tmpl) {
-//       var selectedAnswer = this.value;
-//       return selectedAnswer;
-//     }
-//   });
+Session.setDefault('setAnswer', null);
 
-Template.questItem.helpers  ({
-	setAnswer: function () { return Session.get('setAnswer');
-      },
-	imagePresent: function() {
-	var present = quest.Image_File.findOne(Session.get('selectedQuestId'));
-	if ( present !=== null) {
-		return true
-	};
-	else {return false};
-});
+Template.questItem.events({
+	'click': function (evt, templ){
+		alert("clicked");
+		Session.set("setAnswer", evt.value);
+	},
+   //  'click #answerBtn': function (evt, templ) {
+   //  	alert(templ.find("#answerBtn").value);
+   //  Session.set('setAnswer', 'A');},
+   //  'click #answerBtnB': function (evt, templ) {
+   //  Session.set('setAnswer', 'B');},
+   // 'click #answerBtnC': function (evt, templ) {
+   //  Session.set('setAnswer', 'C');},
+   //  'click #answerBtnD': function (evt, templ) {
+   //  Session.set('setAnswer', 'D');},
+   //  'click #answerBtnE': function (evt, templ) {
+   //  Session.set('setAnswer', 'E');},
+  });
+
+// Template.questItem.helpers  ({
+// 	// setAnswer: function () { return Session.get('setAnswer');
+//  //      },
+// 	// imagePresent: function() {
+// 	// var present = quest.Image_File.findOne(Session.get('selectedQuestId'));
+// 	// if (present !== null) {
+// 	// 	return true;
+// 	// }
+// 	// else {return false};
+// });
+// // recordAnswer = function(answer) {
+// // 	Session set('setAnswer', answer);
+// // }
